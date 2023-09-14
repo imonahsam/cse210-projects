8031c42f82066c153ac26f24c6e8661d8e0d504e
imonahsm <samimonah@gmail.com>
2023-09-14 18:12:32 +0100

    letter grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        String valueFromUser = Console.ReadLine();
+        int x = int.Parse(valueFromUser);
+
+        String letter = " ";
+        
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else if (x <= 60)
+        {
+            letter = "F";
+        }
+
+        // Determine if the user pass or not
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep working hard for next time");
+        }
+
+        // Determine the sign (+, -, or none)
+
+        int lastDigit = (int)(x % 10);
+        String sign = " ";
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Display the letter grade
+        Console.WriteLine($"Your grade letter is: {letter}{sign}");
     }
 }
