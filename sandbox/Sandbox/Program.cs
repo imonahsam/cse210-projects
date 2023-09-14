a51c2a9c236bbe8d63cbee498ff5016c0555fc1a
imonahsm <samimonah@gmail.com>
2023-09-14 11:43:19 +0100

    Asked for name

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#");
+
+       Console.Write("What is your first name? ");
+       String firstname = Console.ReadLine();
+       Console.Write("What is your last name? ");
+       String lastname = Console.ReadLine();
+       Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+
     }
 }
