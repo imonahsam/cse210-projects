e0a53696693504fe04fc1a2ff099d5862b019386
imonahsm <samimonah@gmail.com>
2023-11-06 15:38:56 +0100

    Created shapes dimensions

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Square square = new Square("Green", 5.0);
+        //Console.WriteLine($"Square Colour: {square.GetColour()}");
+        //Console.WriteLine($"Square Area: {square.GetArea()} square units");
+        List<Shape> shapes = new List<Shape>();
+        
+        Rectangle s1 = new Rectangle("Red", 4, 5);
+        shapes.Add(s1);
+
+        Circle s2 = new Circle("Yellow", 6);
+        shapes.Add(s2);
+
+        Square s3 = new Square("Blue", 3);
+        shapes.Add(s3);
+
+        foreach (Shape item in shapes)
+        {
+            string colour = item.GetColour();
+            
+            double area = item.GetArea();
+
+            Console.WriteLine($"The {colour} shape has an area of {area}.");
+        }
     }
 }
